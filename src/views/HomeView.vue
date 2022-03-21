b682ce0db34c18bce36d58f9981f89a494ec8b50
anastasiakonovalenko <konovalenko.a@novaposhta.ua>
2022-03-21 22:58:19 +0200

    configure deployment process

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+<script>
+    import TheWelcome from '@/components/TheWelcome.vue';
+    import { mapMutations, mapState } from 'vuex';
+
+    export default {
+        components: {
+            TheWelcome
+        },
+        data() {
+            return {}
+        },
+        computed: {
+            ...mapState(['count'])
+        },
+        methods: {
+            ...mapMutations(['increment'])
+        }
+    }
 </script>
 
 <template>
   <main>
+      {{ count }}
+      <button @click="increment">hgvjhg</button>
     <TheWelcome />
   </main>
 </template>
